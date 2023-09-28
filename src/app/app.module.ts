e7073074b39f50ec092c3249d0eeefef61b3f093
Naika_Rasquin <rasquin.naika@gmail.com>
2023-09-27 21:28:55 -0300

    actualizacion lista de alumnos

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { MyPComponent } from './components/my-p/my-p.component';
+import { Nombres } from './nombres/nombres.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Nombres,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent,
+    MyPComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
